7df354dfe136cf7cb530afb7adcad758da0e52ed
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-03 05:34:34 +0200

    update

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+app.get("/", (req, res) => {
+  res.send("jobs api");
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
+console.log("we woking on our jobs API");
 const port = process.env.PORT || 3000;
 
 const start = async () => {
